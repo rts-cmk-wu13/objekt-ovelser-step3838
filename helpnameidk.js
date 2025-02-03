3a5275c1be4473167cc321eb28dde9b4a4ec7d76
Stephanie Frost <froststep69@gmail.com>
2025-02-03 14:09:53 +0100

    initial commit

---
+++ helpnameidk.js
@@ -0,0 +1,19 @@
+// Objekt med mindst fire egenskaber
+const person = {
+    navn: "Gorius",
+    alder: "i mellem 81 og 19",
+    by: "København",
+    job: "HE got no"
+};
+
+// Dot notation
+console.log(person.navn);
+console.log(person.alder);
+console.log(person.by);
+console.log(person.job);
+
+// Bracket notation
+console.log(person["navn"]);
+console.log(person["alder"]);
+console.log(person["by"]);
+console.log(person["job"]);
