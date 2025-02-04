64180ca39d388579a9c9ff681869a112d62f5f03
Stephanie Frost <froststep69@gmail.com>
2025-02-04 09:13:53 +0100

    marriage, hobbies and pets

--- helpnameidk.js
+++ helpnameidk.js
@@ -3,7 +3,13 @@ const person = {
     name: "Gorius",
     age: "i mellem 81 og 19",
     city: "København",
-    job: "HE got no"
+    job: "HE got no",
+    married: false, /* boolean */
+    hobbies: ["uhm", "nååååå nice nice nice", "ikke rigtig mere"], /* array */
+    pet: { /* object */
+        type: "watermelon",
+        name: "bob",
+    }
 };
 
 // Dot notation
@@ -11,9 +17,24 @@ console.log(person.name);
 console.log(person.age);
 console.log(person.city);
 console.log(person.job);
+/* console.log(person.married);
+console.log(person.hobbies);
+console.log(person.pet) */
 
 // Bracket notation
 console.log(person["name"]);
 console.log(person["age"]);
 console.log(person["city"]);
 console.log(person["job"]);
+/* console.log(person["married"]);
+console.log(person["hobbies"]);
+console.log(person["pet"]); */
+
+//hobbies funciton
+person.hobbies.forEach(function(hobby) {
+    console.log (hobby)
+})
+
+//pets
+console.log(person.pet.type)
+console.log(person.pet.name)
